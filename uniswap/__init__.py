18fad99c8514a6380d9b17ebda72552482561b4e
Yohan K <72107640+liquid-8@users.noreply.github.com>
2025-12-25 18:45:39 +0200

    Update __init__.py

--- uniswap/__init__.py
+++ uniswap/__init__.py
@@ -1,5 +1,6 @@
 from . import exceptions
 from .cli import main
 from .uniswap import Uniswap, _str_to_addr
+from .uniswap4 import Uniswap4
 
 __all__ = ["Uniswap", "exceptions", "_str_to_addr", "main"]
